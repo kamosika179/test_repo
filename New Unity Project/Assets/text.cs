43bec817ba226ce22e84e3f43e138b58370c50a3
kamosika_note <mayuo253133@gmail.com>
2020-10-05 19:49:25 +0900

    test unity git

---
+++ New Unity Project/Assets/text.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class text : MonoBehaviour
+{
+    Text a;
+    int num;
+    // Start is called before the first frame update
+    void Start()
+    {
+        a = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        num += 1;
+        a.text = num.ToString();
+    }
+}
